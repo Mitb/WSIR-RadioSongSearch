37f32702dc79d22a6755147034d5b0eee05c18b3
Michael Still <michistill@googlemail.com>
2013-05-24 11:56:06 +0200

    try of fix

--- radioNode/application/js/app.js
+++ radioNode/application/js/app.js
@@ -259,12 +259,7 @@ App.SearchResultController = Ember.ObjectController.extend({
     };
     setTimeout(function(){    window.location.reload()}, 0);
     this.transitionToRoute('search', {query: query, page: pageNumber});
-  },
-
-  formerQuery: function(){
-    var re = new RegExp(find, '%20');
-    return query.replace(re, " ");
-  }.property('formerQuery')
+  }
 
 });
 
@@ -278,7 +273,12 @@ App.SearchResultItemView = Ember.View.extend({
         controller.send('showDetails', model);
         App.SearchResultItemView.deselectAll();
         this.set('selected', true);
-    }
+    },
+
+    formerQuery: function(){
+      var re = new RegExp(find, '%20');
+      return query.replace(re, " ");
+    }.property()
 });
 
 App.SearchResultItemView.reopenClass({
